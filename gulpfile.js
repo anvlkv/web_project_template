9685f8759352e6a0bb101899d7a3f7e0d0a2e440
Aleksandr Novolokov <a.nvlkv@gmail.com>
2015-03-10 02:12:22 +0300

    jade, js, sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,83 @@
 var gulp = require('gulp'),     
-    sass = require('gulp-ruby-sass') 
-    notify = require("gulp-notify") 
-    bower = require('gulp-bower');
-	compass = require('gulp-compass');
-	minifyCSS = require('gulp-minify-css');
-	browserSync = require('browser-sync');
+    sass = require('gulp-ruby-sass') ,
+    notify = require('gulp-notify') ,
+    bower = require('gulp-bower'),
+  	compass = require('gulp-compass'),
+  	minifyCSS = require('gulp-minify-css'),
+  	browserSync = require('browser-sync'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    watch = require('gulp-watch'),
+    browserify = require('gulp-browserify'),
+    jade = require('gulp-jade');
 
-var config = {
+var reload = browserSync.reload;
+
+var sources = {
      nodePath: './node_modules',
      bowerDir: './bower_components' ,
-	sassPath: './sass',
+    jade: './jade/**/*.jade',
+  	sass: './sass/**/*.scss',
+    js: './js/**/*.js',
+}
+
+var dist = './dist';
+
+var destinations = {
+    dist: './dist',
+    css: dist + '/css',
+    js: dist + '/js',
+    html: dist + '/',
 }
 
 
+gulp.task('templates', function () {
+    return gulp.src(sources.jade)
+      .pipe(jade({pretty: true}))
+      .pipe(gulp.dest(destinations.html));
+})
+
 gulp.task('compass', function() {
-  gulp.src(config.sassPath + '/*.scss')
+  gulp.src(sources.sass)
     .pipe(compass({
-      config_file: './config.rb',
-      css: 'css',
-      sass: 'sass'
+      style: 'compressed',
+      loadPath: [
+        './resources/sass',
+        sources.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
+      ]
     }))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest(destinations.css));
 });
+
+gulp.task('scripts', function () {
+  return gulp.src(sources.js)
+        .pipe(browserify())
+        .pipe(uglify())
+        .pipe(gulp.dest(destinations.js));
+})
 
 gulp.task('browserSync', function() {
     browserSync({
         server: {
-            baseDir: './'
+            baseDir: destinations.dist
         }
     });
 });
 
 gulp.task('bower', function() { 
     return bower()
-         .pipe(gulp.dest(config.bowerDir)) 
+      .pipe(gulp.dest(sources.bowerDir)) ;
 });
 
  gulp.task('watch', function() {
-     gulp.watch(config.sassPath + '/*.scss', ['css']); 
+     gulp.watch(sources.sass, ['compass', reload]); 
+    gulp.watch(sources.jade, ['templates', reload]);
+    gulp.watch(sources.js, ['scripts', reload]);
 });
 
-  gulp.task('default', ['bower', 'browserSync', 'compass']);
+  gulp.task('default', ['bower', 'browserSync','watch']);
+
+
+
+
